18ecb5249c81c59cddb3d0276220e1cc711a3e68
Jason C <jayingmantis.555@gmail.com>
2021-09-11 15:49:25 -0500

    added delete route

--- controllers/users.js
+++ controllers/users.js
@@ -25,10 +25,18 @@ router.get('/seed', (req, res) => {
       res.status(400).json({ message: error.message })
     }
   })
-
 //READ
   /*Index*/
     router.get('/', async (req, res) => {
+      try{
+        const foundUsers = await User.find({})
+        res.status(200).json(foundUsers)
+      }catch(error){
+        console.error(error)
+        res.status(400).json({message: error.message})
+      }
+    })
+    router.get('/login', async (req, res) => {
       try{
         const foundUsers = await User.find({})
         res.status(200).json(foundUsers)
@@ -41,5 +49,15 @@ router.get('/seed', (req, res) => {
 
 
 //DELETE
+router.delete('/:id', async (req, res) => {
+  try{
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    res.status(200).json(deletedUser);
+  }catch(error){
+    console.error(error);
+    res.status(400).json({ message: error.message})
+  }
+})
+
 
 module.exports = router;
