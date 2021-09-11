501f9939b3209bd64abe461f5f8c42eeec982053
Jason C <jayingmantis.555@gmail.com>
2021-09-11 12:23:23 -0500

    feature user model updated

--- models/user.js
+++ models/user.js
@@ -1,1 +1,13 @@
-/*push test*/
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+  username: {type: String, required: true},
+  email: {type: String, required: true},
+  password: {type: String, required: true},
+  dateOfBirth: {type: Date, required: true},
+  zipCode: {type: String, required: true},
+  doctorName: {type: String, required: true},
+  providerLocation: {type: String, required: true},
+})
+
+module.exports = model('User', userSchema)
