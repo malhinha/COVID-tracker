eac4988c41662bd476a815a822e35cdd9185bc38
Jason C <jayingmantis.555@gmail.com>
2021-09-11 15:03:08 -0500

    seed route working. POST and GET route for '/' working

---
+++ controllers/users.js
@@ -0,0 +1,45 @@
+const User = require('../models/user.js');
+const router = require('express').Router();
+
+
+//Seed route <---example for testing
+router.get('/seed', (req, res) => {
+  User.create({
+    username: "superman",
+    email: "superman@gmail.com",
+    password: "passwordtest",
+    zipCode: "05101",
+    doctorName: "doctortest",
+    providerLocation: "123 Main St"
+  })
+})
+
+
+//CREATE
+  router.post('/', async(req, res) => {
+    try{
+      const createdUser = await User.create(req.body)
+      res.status(200).json(createdUser)
+    }catch(error){
+      console.error(error)
+      res.status(400).json({ message: error.message })
+    }
+  })
+
+//READ
+  /*Index*/
+    router.get('/', async (req, res) => {
+      try{
+        const foundUsers = await User.find({})
+        res.status(200).json(foundUsers)
+      }catch(error){
+        console.error(error)
+        res.status(400).json({message: error.message})
+      }
+    })
+//UPDATE
+
+
+//DELETE
+
+module.exports = router;
