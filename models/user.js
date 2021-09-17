8208a5c2b0f5e8e6be0831007e78bc6182383525
Jason C <jayingmantis.555@gmail.com>
2021-09-17 14:00:44 -0500

    updated user model so that 'email' is unique

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const { Schema, model } = require('mongoose');
 const statesArray = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"];
 
 const userSchema = new Schema({
-  email: {type: String, required: true},
+  email: {type: String, unique: true, required: true},
   password: {type: String, required: true},
   firstName: {type: String, required: true},
   lastName: {type: String, required: true},
