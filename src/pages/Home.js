2b34a969574b52950686451c2034854933dde258
Neet <navneetkaur3@gmail.com>
2021-09-19 02:37:22 -0500

    Fixed Register and Login, introduced Symptom and Information page, updated header for reflecting user information, introduced logout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,8 +6,13 @@ export default function Home(props) {
 	return (
 		<div>
 			<Header />
-			<h3>Login Covid 19 Symptoms</h3>
-			<h3>Information & Recommendations</h3>
+			<Link to={`/logSymptoms`}>
+				<h3>Login Covid 19 Symptoms</h3>
+			</Link>
+
+			<Link to={`/information`}>
+				<h3>Information & Recommendations</h3>
+			</Link>
 		</div>
 	);
 }
