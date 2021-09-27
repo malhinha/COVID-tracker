3a91d15e5ea450726b797a0943c038e89bd5a429
John Malhinha <john.malhinha@gmail.com>
2021-09-26 19:32:06 -0700

    Fixed bug where button label was not being presented.

--- src/components/LogSym/Button.js
+++ src/components/LogSym/Button.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default function Button(props) {
 	return (
 		<>
-			<button id={props.id} type={props.type} value={props.value}></button>
+			<button id={props.id} type={props.type} value={props.value}>
+				{props.value}
+			</button>
 		</>
 	);
 }
